4b5c3fffa6dd34ca30d5b68cc0a74f15e7431a07
Khaz1 <panesh-1994@mail.ru>
2024-02-19 15:28:30 +0300

    add all project with all tasks

---
+++ app/src/main/java/com/example/myfirstapplication/di/ApplicationScope.kt
@@ -0,0 +1,7 @@
+package com.example.myfirstapplication.di
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationScope
---
+++ app/src/main/java/com/example/myfirstapplication/presentation/adapters/CoinInfoDiffCallback.kt
@@ -0,0 +1,15 @@
+package com.example.myfirstapplication.presentation.adapters
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.myfirstapplication.domain.CoinInfo
+
+object CoinInfoDiffCallback : DiffUtil.ItemCallback<CoinInfo>() {
+
+    override fun areItemsTheSame(oldItem: CoinInfo, newItem: CoinInfo): Boolean {
+        return oldItem.fromSymbol == newItem.fromSymbol
+    }
+
+    override fun areContentsTheSame(oldItem: CoinInfo, newItem: CoinInfo): Boolean {
+        return oldItem == newItem
+    }
+}
